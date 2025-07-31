b51ced317f295c7d6f7c80ace56bab6c659157b6
Artem Z. <107121930+azakhardev@users.noreply.github.com>
2025-07-31 12:40:06 +0200

    Add files via upload

---
+++ RealEstate_Server/Attributes/BadCustomerNameAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Server.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace RealEstate_Server.Attributes
+{
+    public class BadCustomerNameAttribute : ValidationAttribute
+    {
+        MyContext context = new MyContext();
+
+        public override bool IsValid(object? value)
+        {
+            this.ErrorMessage = "Username is already used";
+            foreach (Customer cstmr in context.Customers)
+            {
+                if (value.ToString() == cstmr.Username)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ RealEstate_Server/Attributes/BadUserNameAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Server.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace RealEstate_Server.Attributes
+{
+    public class BadUserNameAttribute : ValidationAttribute
+    {
+        MyContext context = new MyContext();
+
+        public override bool IsValid(object? value)
+        {
+            this.ErrorMessage = "Username is already used";
+            foreach (User usr in context.Users)
+            {
+                if (value.ToString() == usr.Username)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
