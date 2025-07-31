e485b910499b6da3ff33c0c7db0aa5d04d6b5ffe
Artem Z. <107121930+azakhardev@users.noreply.github.com>
2025-07-31 12:44:04 +0200

    Add files via upload

---
+++ RealEstate_Server/wwwroot/MyJs/Detail.js
@@ -0,0 +1,40 @@
+﻿let pictures = document.querySelectorAll('.photoSmall img');
+let picturesDiv = document.querySelectorAll('.photoSmall');
+let mainSmallPicture = document.querySelector('.mainPhotoSmall');
+let i = 0;
+picturesDiv.forEach(p =>
+{
+    if (i > 4)
+    {
+        p.style.display = "none";
+    }
+    i++;
+})
+
+pictures.forEach(clickedPicture => {
+    clickedPicture.addEventListener('click', () => {
+        const mainPicture = document.querySelector('.photoBig img');
+        mainPicture.setAttribute('src', clickedPicture.src);
+    });
+});
+
+let number = 5;
+let next = document.querySelector('div .nextPhotos')
+let prev = document.querySelector('div .previousPhotos')
+
+next.addEventListener('click', () => {          
+    if (number + 1 < pictures.length + 1) {
+        picturesDiv[number - 5].style.display = "none";
+        picturesDiv[number].style.display = "block";
+        number += 1;
+    }
+})
+
+prev.addEventListener('click', () => {
+
+    if (number - 1 >= 5) {
+        number -= 1;
+        picturesDiv[number].style.display = "none";
+        picturesDiv[number - 5].style.display = "block";                   
+    }
+})
---
+++ RealEstate_Server/wwwroot/MyJs/Layout.js
@@ -0,0 +1,17 @@
+﻿const tools = document.querySelectorAll('.Tools a')
+
+tools.forEach((t) => {
+    t.addEventListener('click', () => {
+        CloseAll()        
+        t.classList.add('Current');
+        console.log('clicked')
+    });
+});
+
+function CloseAll()
+{
+    document.querySelectorAll('.Tools a').forEach((a) =>
+    {
+        a.classList.remove('Current');
+    })
+}
